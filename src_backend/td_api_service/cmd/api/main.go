6e215f0461149b6a72ade27292501f9f6f2ce038
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-01-23 16:24:04 +0700

    chuyển cấp thư mục

--- src_backend/td_api_service/cmd/api/main.go
+++ src_backend/td_api_service/cmd/api/main.go
@@ -2,36 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
-	"td_api_service/internal/controller"
-	"td_api_service/internal/middleware"
-	"td_api_service/internal/service"
+	"td_api_service/external"
 )
 
 func main() {
 	port := flag.Int("port", 7777, "Port to run the server")
 	flag.Parse()
-
-	// Khởi tạo router của thư viện chuẩn (Go 1.22+)
-	mux := http.NewServeMux()
-
-	apiSvc := service.NewAPITestService()
-	apiCtrl := controller.NewAPIController(apiSvc)
-
-	// Routing cực gọn
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Ok")
-	})
-	mux.HandleFunc("POST /exec", apiCtrl.Execute)
-
-	// Xâu chuỗi Middlewares: Logger -> CORS -> Router
-	finalHandler := middleware.Logger(middleware.CORS(mux))
-
-	addr := fmt.Sprintf(":%d", *port)
-	fmt.Printf("Server API đang chạy tại %s\n", addr)
-
-	if err := http.ListenAndServe(addr, finalHandler); err != nil {
-		panic(err)
-	}
+	external.BuildAPIRoute(port)
 }
---
+++ src_backend/td_api_service/external/api_route.go
@@ -0,0 +1,34 @@
+package external
+
+import (
+	"fmt"
+	"net/http"
+	"td_api_service/internal/controller"
+	"td_api_service/internal/middleware"
+	"td_api_service/internal/service"
+)
+
+func BuildAPIRoute(port *int) {
+
+	// Khởi tạo router của thư viện chuẩn (Go 1.22+)
+	mux := http.NewServeMux()
+
+	apiSvc := service.NewAPITestService()
+	apiCtrl := controller.NewAPIController(apiSvc)
+
+	// Routing cực gọn
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Ok")
+	})
+	mux.HandleFunc("POST /exec", apiCtrl.Execute)
+
+	// Xâu chuỗi Middlewares: Logger -> CORS -> Router
+	finalHandler := middleware.Logger(middleware.CORS(mux))
+
+	addr := fmt.Sprintf(":%d", *port)
+	fmt.Printf("Server API đang chạy tại %s\n", addr)
+
+	if err := http.ListenAndServe(addr, finalHandler); err != nil {
+		panic(err)
+	}
+}
