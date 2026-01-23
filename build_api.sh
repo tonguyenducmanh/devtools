4fe4317840875c102fb35707be2a1cf754e87701
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-01-23 15:54:02 +0700

    bổ sung hàm build api

---
+++ build_api.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+# Đường dẫn tuyệt đối hoặc tương đối tính từ thư mục chạy script
+MODULE_DIR="./src_backend/td_api_service"
+APP_NAME="./out/tool-tomanh-api"
+
+# Di chuyển vào thư mục module để Go nhận diện go.mod
+cd $MODULE_DIR
+
+# Build cho các nền tảng
+echo "Building for Mac Intel..."
+GOOS=darwin GOARCH=amd64 go build -o ../../${APP_NAME}-mac-intel ./cmd/api/main.go
+
+echo "Building for Mac M1/M2/M3..."
+GOOS=darwin GOARCH=arm64 go build -o ../../${APP_NAME}-mac-arm ./cmd/api/main.go
+
+echo "Building for Linux..."
+GOOS=linux GOARCH=amd64 go build -o ../../${APP_NAME}-linux ./cmd/api/main.go
+
+echo "Building for Windows..."
+GOOS=windows GOARCH=amd64 go build -o ../../${APP_NAME}.exe ./cmd/api/main.go
+
+echo "Build thành công!"
