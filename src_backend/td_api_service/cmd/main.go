38163bc12737485a1b874a20cdcce42fca5d5c98
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-01-23 20:26:40 +0700

    .

--- src_backend/td_api_service/cmd/main.go
+++ src_backend/td_api_service/cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	port := flag.Int("port", 7777, "Port to run the server")
-	trace := flag.Bool("api-trace", false, "Hiển thị log chi tiết cho Web server")
+	trace := flag.Bool("trace", false, "Hiển thị log chi tiết cho Web server")
 	flag.Parse()
 	api_app.BuildAPIRoute(port, trace)
 }
