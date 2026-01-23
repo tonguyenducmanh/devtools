309db89e62b6ab6e126c18490e5852e5d162a851
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-01-23 17:10:47 +0700

    add command

--- src_backend/td_daemon_app/cmd/main.go
+++ src_backend/td_daemon_app/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"td_api_service/external"
+)
 
+const banner = `
+  ______ ____  ____  __       ______ ____  __  ___ ___     _   __ __  __
+ /_  __// __ \/ __ \/ /      /_  __// __ \/  |/  //   |   / | / // / / /
+  / /  / / / / / / / /        / /  / / / / /|_/ // /| |  /  |/ // /_/ / 
+ / /  / /_/ / /_/ / /___     / /  / /_/ / /  / // ___ | / /|  // __  /  
+/_/   \____/\____/_____/    /_/   \____/_/  /_//_/  |_|/_/ |_//_/ /_/   
+
+From TDManh with luv
+
+`
+
+func main() {
+	fmt.Print(banner)
+	// Logic ứng dụng của bạn ở đây
+	port := flag.Int("port", 7777, "Port to run the server")
+	flag.Parse()
+	external.BuildAPIRoute(port)
 }
--- src_backend/td_daemon_app/go.mod
+++ src_backend/td_daemon_app/go.mod
@@ -3,3 +3,5 @@ module td_daemon_app
 go 1.25.6
 
 replace td_api_service => ../td_api_service
+
+require td_api_service v0.0.0-00010101000000-000000000000
