b5ed82e5a6f420b110f271626f0505c36668ff73
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-01-24 07:33:23 +0700

    đổi sang hàm chung để print banner

--- src_backend/td_app/cmd/api_app/main.go
+++ src_backend/td_app/cmd/api_app/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"td_app/internal/banner"
 	apiApp "td_core_service/external/api_app"
 )
 
 func main() {
+	banner.PrintBanner()
 	port := flag.Int("port", 7777, "Port to run the server")
 	trace := flag.Bool("trace", false, "Hiển thị log chi tiết cho Web server")
 	flag.Parse()
--- src_backend/td_app/cmd/daemon_app/main.go
+++ src_backend/td_app/cmd/daemon_app/main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"sync"
+	"td_app/internal/banner"
 	apiApp "td_app/internal/api_app"
 	webApp "td_app/internal/web_app"
 )
 
-const banner = `
-  ______ ____  ____  __       ______ ____  __  ___ ___     _   __ __  __
- /_  __// __ \/ __ \/ /      /_  __// __ \/  |/  //   |   / | / // / / /
-  / /  / / / / / / / /        / /  / / / / /|_/ // /| |  /  |/ // /_/ / 
- / /  / /_/ / /_/ / /___     / /  / /_/ / /  / // ___ | / /|  // __  /  
-/_/   \____/\____/_____/    /_/   \____/_/  /_//_/  |_|/_/ |_//_/ /_/   
-
-From TDManh with luv
-
-`
-
 func main() {
-	fmt.Print(banner)
+	banner.PrintBanner()
 
 	var wg sync.WaitGroup
 	wg.Add(2)
