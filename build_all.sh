8185cf03d7db321dc66ff1fcc03b1ddaf9aa8bcd
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-01-27 20:54:57 +0700

    update build

--- build_all.sh
+++ build_all.sh
@@ -4,3 +4,10 @@ chmod 777 ./scripts/build_daemon.sh
 ./scripts/build_web_for_daemon.sh
 ./scripts/build_api.sh
 ./scripts/build_daemon.sh
+
+# Copy toàn bộ nội dung config vào output
+ROOT_DIR=$(pwd)
+OUTPUT_DIR="$ROOT_DIR/out"
+CONIG_DIR="$ROOT_DIR/config"
+
+cp -r "$CONIG_DIR"/. "$OUTPUT_DIR"/
